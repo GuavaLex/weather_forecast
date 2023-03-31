5a4bb74ccbb7cc369edbb2a8140a37df20fa5516
guava <guavareigen2@gmail.com>
2023-03-31 12:08:31 +0530

    Completed App

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select Data to view", ("Temperature", "Sky"))
 sub = st.subheader(f"{option} for the next {days} days in {place}")
 
-get_data(place,days,option)
+if place:
 
-dates = ["2022-25-20", "2022-26-20", "2022-27-20"]
-temperature = [10,11,12]
-figure = px.line(x=dates,y=temperature,labels={"x":"Dates","y":"temperature"})
-st.plotly_chart(figure)
+    filtered_data = get_data(place, days)
+
+    if option == "Temperature":
+        tempertures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"]for  dict in filtered_data]
+        figure = px.line(x=dates, y=tempertures, labels={"x": "Dates", "y": "temperature"})
+        st.plotly_chart(figure)
+    if option == "Sky":
+        images = {"Clear":"images/clear.png","Clouds":"images/cloud.png","Rain":"images/rain.png","Snow":"images/snow.png"}
+        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_condition]
+        st.image(image_paths, width=115)
